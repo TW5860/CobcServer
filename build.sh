afeea78c663a4bea66e91cf04a5c7ee412417623
Simon Weiler <sweiler@thoughtworks.com>
2018-08-03 10:39:08 +0200

    add cleanup code to build script

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 ID=$1
-rm -f ./scripts/test-${ID}
+rm -f scripts/test-${ID}
 cobc -x -free -o scripts/test-${ID} scripts/testdriver-${ID}.cbl
 ./scripts/test-${ID}
+rm -f scripts/test-${ID}
+rm -f scripts/testdriver-${ID}.cbl
